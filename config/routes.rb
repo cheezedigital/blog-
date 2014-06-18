664b6b0db89e6cfd2beb973030495decd63df10d
Justin Veirs <justinveirs@gmail.com>
2014-06-18 15:17:17 -0600

    edits posts/index, posts/show and posts/new as well as the controller actions. added a static controller with views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  root 'static#home'
 
-  get 'posts/show'
+  get '/about', to: 'static#about', as: :about 
 
-  get 'posts/new'
+    resources :posts
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
